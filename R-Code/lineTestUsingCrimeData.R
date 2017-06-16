184bda0ed7f7c8995062849f637838345157b6e7
Preston Bruce <pb.itoldyouso@gmail.com>
2017-06-16 15:46:10 -0500

    code

--- R-Code/lineTestUsingCrimeData.R
+++ R-Code/lineTestUsingCrimeData.R
@@ -8,7 +8,10 @@ library(lattice)
 CityDeaths <- read.table("../Datasets/Deaths_in_122_U.S._cities_-_1962-2016._122_Cities_Mortality_Reporting_System.csv",sep = ",", header = TRUE, stringsAsFactors = FALSE)
 deathsByAge <- select(CityDeaths, Year, State, City, All.Deaths, X.1.year..all.cause.deaths., X1.24.years..all.cause.deaths., X25.44.years, X45.64.years..all.cause.deaths., X65..years..all.cause.deaths.)
 
-cityDeathModel <- 
+cityDeathModel <- lm(deathsByAge$State ~ deathsByAge$All.Deaths + deathsByAge$City)
+cityDeathModel2 <- lm(deathsByAge$State ~ deathsByAge$All.Deaths * deathsByAge$City)
+summary(cityDeathModel)
+summary(cityDeathModel2)
 
 pdf("../Results/R/CrimeTime.pdf")
 ggplot(data = deathsByAge,mapping= aes(x = City, y = All.Deaths))+
