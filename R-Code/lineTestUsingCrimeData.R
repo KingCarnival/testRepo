72bcdabcb03780f56b3e3c4b50ee195f0d903e1e
Preston Bruce <pb.itoldyouso@gmail.com>
2017-07-07 13:22:53 -0500

    updates

--- R-Code/lineTestUsingCrimeData.R
+++ R-Code/lineTestUsingCrimeData.R
@@ -5,6 +5,7 @@ library(lme4)
 
 CityDeaths <- read.table("../Datasets/Deaths_in_122_U.S._cities_-_1962-2016._122_Cities_Mortality_Reporting_System.csv",sep = ",", header = TRUE, stringsAsFactors = FALSE)
 deathsByAge <- select(CityDeaths, Year, State, City, All.Deaths, X.1.year..all.cause.deaths., X1.24.years..all.cause.deaths., X25.44.years, X45.64.years..all.cause.deaths., X65..years..all.cause.deaths.)
+as_tibble(CityDeaths)
 
 mixedModelTest <- lmer(CityDeaths$REGION + CityDeaths$All.Deaths * (1 | CityDeaths$Pneumonia.and.Influenza.Deaths))
 
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 # This repo is a collection of files used to test programs
 
+Please excuse the mess.
